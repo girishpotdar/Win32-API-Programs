f801f8b086f440ad09c6e1499ffb33c8089435f6
Girish Potdar <girishpotdar41@gmail.com>
2021-09-08 17:51:12 +0530

    DLL with export

---
+++ 02-Exporting/02-UsingDefFile/MyMath.cpp
@@ -0,0 +1,38 @@
+#include <Windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hDll, DWORD dwReason, LPVOID lpReserved)
+{
+	// code
+	// dll injection / API hooking can be done with below cases :) 
+	switch (dwReason) 
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+
+	case DLL_THREAD_ATTACH:
+		break;
+
+	case DLL_THREAD_DETACH:
+		break;
+
+	case DLL_PROCESS_DETACH:
+		break;
+
+	default:
+		break;
+
+	}
+
+	return TRUE;
+
+}
+
+
+//declaration specific --> decl spec --> __declspec keyword is Microsoft specific
+extern "C" int MakeCube(int num)
+{
+	//code
+	int cube = num * num * num;
+
+	return cube;
+}
